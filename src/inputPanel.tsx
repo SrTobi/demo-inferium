646dc9609efa48ac61dac646d9b4cca5f5e343ab
SrTobi <code.databyte@gmail.com>
2018-01-10 15:26:25 +0100

    added predefs

--- src/inputPanel.tsx
+++ src/inputPanel.tsx
@@ -5,6 +5,7 @@ import * as Visual from './visuals';
 import {observable} from 'mobx'
 import * as mobx from 'mobx-react'
 import MonacoEditor from 'react-monaco-editor';
+import {predefs} from './predefs'
 import './inferium-web-fastopt'
 
 declare namespace Inferium {
@@ -25,12 +26,32 @@ export class InputPanel extends React.Component<{}, {}> {
     @observable
     private error: string | undefined = undefined
 
+    @observable
+    private example: string
+
+    constructor() {
+        super({})
+
+        this.code = predefs[0].code
+    }
+
     private infer(code: string) {
         this.error = ""
         try {
             this.result = Inferium.generateTypeDefinition(code)
         } catch(e) {
             this.error = e.toString()
+        }
+    }
+
+    private onChangeExample(ev: React.ChangeEvent<HTMLSelectElement>) {
+        const value = ev.target.value
+
+        this.example = value
+        const predef = predefs.find(p => p.name == value)
+
+        if (predef) {
+            this.code = predef.code
         }
     }
 
@@ -48,9 +69,21 @@ export class InputPanel extends React.Component<{}, {}> {
                                 />
                     </div>
                 </div>
+                <div style={{textAlign: "center"}}>
+                    Be careful... recursion does not work at the moment... this is an alpha version after all 
+                </div>
                 <div className="panel panel-default config-panel">
-                    <button type="button" onClick={() => this.infer(this.code)}>Infer</button>
-                    <span className="error-box"> {this.error? "Error: " + this.error : ""}</span>
+                    <span>
+                        <button type="button" onClick={() => this.infer(this.code)}>Infer</button>
+                        <span className="error-box"> {this.error? "Error: " + this.error : ""}</span>
+                    </span>
+
+                    <span style={{float: "right"}}>
+                        <span>Examples: </span>
+                        <select onChange={this.onChangeExample.bind(this)}>
+                            {predefs.map(predef => (<option value={predef.name}>{predef.name}</option>))}
+                        </select>
+                    </span>
                 </div>
                 <div className="panel panel-default config-panel">
                     <div className="panel-body collapse in">
---
+++ src/predefs.ts
@@ -0,0 +1,87 @@
+
+
+
+export class Predef {
+    constructor(public readonly name: string, public readonly code: string) {
+    }
+}
+
+
+const StackCode = new Predef("Stack", `
+var elements = undefined
+return {
+    push: (e) => {
+        elements = {
+            next: elements,
+            item: e
+        }
+    },
+
+    pop: () => {
+        var result = elements.item
+        elements = elements.next
+        return result
+    }
+}
+`)
+
+const StackInnerCode = new Predef("Stack with inner", `
+var elements = undefined
+return {
+    push: (e) => {
+        elements = {
+            next: elements,
+            item: e
+        }
+    },
+
+    pop: () => {
+        var result = elements.item
+        elements = elements.next
+        return result
+    },
+
+    inner: () => {
+        return elements
+    }
+}
+`)
+
+const IdentityCode = new Predef("Identity", `
+return (anything) => {
+    return anything
+}
+`)
+
+
+const ObjectFilteringCode = new Predef("Object filtering", `
+var a = { cond: true, prop: "true" }
+var b = { cond: false, prop: "false" }
+
+// rand is a global variable that is either true or false
+if (rand) {
+  var x = a
+} else {
+  x = b
+}
+
+// here, x can be a or b
+var cond = x.cond
+if (cond) {
+  // here, x can only be a
+  x.prop = "haha"
+}
+
+return x.prop
+`)
+
+const FoldCode = new Predef("Fold", `
+return (folder, list, init) => {
+    // there are no lists :) and also no loop and no recursion (yet)
+    // so do it kinda manually
+    var acc = folder(init, list)
+    return folder(acc, list)
+}
+`)
+
+export const predefs: Predef[] = [StackCode, StackInnerCode, IdentityCode, ObjectFilteringCode, FoldCode]
