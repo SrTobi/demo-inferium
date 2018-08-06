e7a9e61063515a6cfb44e216d4623661690d218b
SrTobi <code.databyte@gmail.com>
2018-08-06 17:24:24 +0200

    fixed examples

--- src/predefs.ts
+++ src/predefs.ts
@@ -7,48 +7,60 @@ export class Predef {
 }
 
 
+const EmitterCode = new Predef("Emitter", `
+function createEmitter() {
+    var handlers = undefined
+    return {
+        subscribe: (handler) => {
+            let o = {}
+            o.next = handlers
+            o.handler = handler
+            handlers = o
+        },
+
+        emit: (e) => {
+            for (let cur = handlers; cur; cur = cur.next) {
+                cur.handler(e)
+            }
+        }
+    }
+}
+
+exports.createEmitter = createEmitter
+`)
+
+const EmitterInstantiationCode = new Predef("Emitter instantiation", `
+function createEmitter() {
+    var handlers = undefined
+    return {
+        subscribe: (handler) => {
+            let o = {}
+            o.next = handlers
+            o.handler = handler
+            handlers = o
+        },
+
+        emit: (e) => {
+            for (let cur = handlers; cur; cur = cur.next) {
+                cur.handler(e)
+            }
+        }
+    }
+}
+
+const emitter = createEmitter()
+let result = "Hello World"
+emitter.subscribe((e) => {result = e})
+
+emitter.emit("Reset")
+result = undefined
+emitter.emit("Reset again")
+
+exports.result = result
+`)
+
 const StackCode = new Predef("Stack", `
-var elements = undefined
-return {
-    push: (e) => {
-        elements = {
-            next: elements,
-            item: e
-        }
-    },
-
-    pop: () => {
-        var result = elements.item
-        elements = elements.next
-        return result
-    }
-}
-`)
-
-const StackInnerCode = new Predef("Stack with inner", `
-var elements = undefined
-return {
-    push: (e) => {
-        elements = {
-            next: elements,
-            item: e
-        }
-    },
-
-    pop: () => {
-        var result = elements.item
-        elements = elements.next
-        return result
-    },
-
-    inner: () => {
-        return elements
-    }
-}
-`)
-
-const StackInstantiationCode = new Predef("Stack instantiation", `
-var createStack = () => {
+function createStack() {
     var elements = undefined
     return {
         push: (e) => {
@@ -62,75 +74,36 @@ var createStack = () => {
             var result = elements.item
             elements = elements.next
             return result
-        },
-
-        top: () => {
-            return elements.item
         }
     }
 }
 
-var stack = createStack()
-stack.push(3)
-stack.push("test")
-
-var result = {}
-result.a = stack.pop()
-result.b = stack.top()
-result.c = stack.pop()
-return result
+exports.createStack = createStack
 `)
 
 const IdentityCode = new Predef("Identity", `
-return (anything) => {
+exports.id = function(anything) {
     return anything
 }
 `)
 
-
-const ObjectFilteringCode = new Predef("Object filtering", `
-var a = { cond: true, prop: "true" }
-var b = { cond: false, prop: "false" }
-
-// rand is a global variable that is either true or false
-if (rand) {
-  var x = a
-} else {
-  x = b
-}
-
-// here, x can be a or b
-var cond = x.cond
-if (cond) {
-  // here, x can only be a
-  x.prop = "haha"
-}
-
-return x.prop
-`)
-
-const SimpleRecursion = new Predef("Simple Recursion", `
-
-`)
-
 const FoldCode = new Predef("Fold", `
-var fold = (folder, it, init) => {
-    if (it.hasNext()) {
-        return fold(folder, it, folder(it.next(), init))
-    } else {
-        return init
+function fold(f, it, init) {
+    let temp = init
+    while (it.hasNext()) {
+        temp = f(temp, it.next())
     }
+    return temp
 }
 
-return fold
+exports.fold = fold
 `)
 
 const EndlessRecursion = new Predef("Endless recursion", `
-var f = () => {
+function f() {
     return f()
 }
-
-return f
+exports.noEnd = f
 `)
 
-export const predefs: Predef[] = [StackCode, StackInnerCode, StackInstantiationCode, IdentityCode, ObjectFilteringCode, FoldCode, EndlessRecursion]
+export const predefs: Predef[] = [EmitterCode, EmitterInstantiationCode, StackCode, IdentityCode, FoldCode, EndlessRecursion]
